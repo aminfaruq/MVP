2f067e88f91ad403f8903453dce05440ddfde75d
M Amin Faruq <maminfaruq@MacBook-Pro-M.local>
2019-01-05 14:19:27 +0700

    Initial commit

---
+++ app/src/main/java/id/co/maminfaruq/mvp/MainModel.java
@@ -0,0 +1,13 @@
+package id.co.maminfaruq.mvp;
+
+public class MainModel {
+    private double volume;
+
+    public MainModel(double volume) {
+        this.volume = volume;
+    }
+
+    public double getVolume() {
+        return volume;
+    }
+}
---
+++ app/src/main/java/id/co/maminfaruq/mvp/MainView.java
@@ -0,0 +1,5 @@
+package id.co.maminfaruq.mvp;
+
+public interface MainView {
+    void tampilVolume(MainModel model);
+}
